76447bc57dd2da7216a00eb7b0b6e112a1734617
karan <kkadam58707@gmail.com>
2024-09-25 23:21:31 +0530

    all files upload

---
+++ firebase-auth-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthComponent from './AuthComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Firebase Auth React App</h1>
+      <AuthComponent />
+    </div>
+  );
+}
+
+export default App;
